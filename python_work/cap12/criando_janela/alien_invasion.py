c5b0d09b0e266a5ccfc34fbccd330a7a240bf123
GledsonVC <gledsonvc@gmail.com>
2024-05-15 18:42:53 -0300

    cap12 criando janela alien_invasion

---
+++ python_work/cap12/criando_janela/alien_invasion.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Classe geral para gerenciar ativos e comportamento do jogo"""
+
+    def __init__(self):
+        """Inicializa o jogo e cria recursos do jogo"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+    
+    
+    def rum_game(self):
+        """Inicia o loop principal do jogo"""
+        while True:
+            # Observa eventos de teclado e mouse
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            
+            # Deixa a tela desenhada mais recente visível
+            pygame.display.flip()
+            if __name__ == '__main__':
+                # Cria uma instancia do jogo e execute o jogo
+                ai = AlienInvasion
+                ai.rum_game()
