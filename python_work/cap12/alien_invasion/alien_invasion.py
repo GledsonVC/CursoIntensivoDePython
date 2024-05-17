9b0502ca1ef304dafff32bb5efe97ce2c4c4ff26
Gledson Vasconcellos Cavalheiro <gledsonvc@gmail.com>
2024-05-16 22:28:15 -0300

    Definindo cor de fundo e Criando uma classe Settings

--- python_work/cap12/alien_invasion/alien_invasion.py
+++ python_work/cap12/alien_invasion/alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Classe geral para gerenciar ativos e comportamento do jogo"""
 
@@ -8,11 +10,19 @@ class AlienInvasion:
         """Inicializa o jogo e cria recursos do jogo"""
         pygame.init()
         self.clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.settings = Settings()
+        
+
+        #self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-    
-    
-    def rum_game(self):
+
+        # Define a cor do background.
+        #self.bg_color = (230, 230, 230)
+
+
+    def run_game(self):
         """Inicia o loop principal do jogo"""
         while True:
             # Observa eventos de teclado e mouse
@@ -20,6 +30,11 @@ class AlienInvasion:
                 if event.type == pygame.QUIT:
                     sys.exit()
             
+            # Redesenha a tela durante cada passagem pelo loop
+            #self.screen.fill(self.bg_color)
+            self.screen.fill(self.settings.bg_color)
+            
+
             # Deixa a tela desenhada mais recente visível
             pygame.display.flip()
             self.clock.tick(60)
@@ -27,4 +42,4 @@ class AlienInvasion:
 if __name__ == '__main__':
     # Cria uma instancia do jogo e execute o jogo
     ai = AlienInvasion()
-    ai.rum_game()
+    ai.run_game()
