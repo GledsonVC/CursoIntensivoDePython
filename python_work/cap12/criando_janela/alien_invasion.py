aac90eaf9b3f93d8c36d44b8ceac835097a7a283
Gledson Vasconcellos Cavalheiro <gledsonvc@gmail.com>
2024-05-16 21:04:11 -0300

    cap12 Controlando taxa de frame

--- python_work/cap12/criando_janela/alien_invasion.py
+++ python_work/cap12/criando_janela/alien_invasion.py
@@ -7,7 +7,7 @@ class AlienInvasion:
     def __init__(self):
         """Inicializa o jogo e cria recursos do jogo"""
         pygame.init()
-        #self.clock = pygame.time.Clock()
+        self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
     
@@ -22,7 +22,7 @@ class AlienInvasion:
             
             # Deixa a tela desenhada mais recente visível
             pygame.display.flip()
-
+            self.clock.tick(60)
 
 if __name__ == '__main__':
     # Cria uma instancia do jogo e execute o jogo
