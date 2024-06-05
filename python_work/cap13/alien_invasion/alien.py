fed4b23157857eeb83a02dce75baf277934d17ad
GledsonVC <gledsonvc@gmail.com>
2024-06-05 17:42:24 -0300

    cap13 Criando Verificando se um alienígena alcançou a borda

--- python_work/cap13/alien_invasion/alien.py
+++ python_work/cap13/alien_invasion/alien.py
@@ -23,8 +23,14 @@ class Alien(Sprite):
         # Armazena a posição horizontal exata do alienígena
         self.x = float(self.rect.x)
 
+        def check_edges(self):
+            """Retorna True se a alienígenas estiver na borda da tela"""
+            screen_rect = self.screen.get_rect()
+            return (
+                self.rect.right >= screen_rect.right) or (self.rect.left <=0)
+        
         def update(self):
-            """Move o alienígena para a direita"""
-            self.x += self.settings.alien_speed
+            """Desloca o alienigena para a direita ou para a esquerda"""
+            self.x += self.settings.alien_speed * self.settings.fleet_direction
             self.rect.x = self.x
         
